2ee556b970c113be245806ef7a86982af0f81aed
bezzpezz <bezz.pezz@hotmail.com>
2024-02-04 00:13:38 +1000

    reverted

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -19,13 +19,4 @@ function MyApp({ Component, pageProps }: AppProps) {
     </>
 }
 
-/* eslint-disable @next/next/no-sync-scripts */
-/* import '@/styles/globals.css'
-
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
-} */
-
 export default MyApp
